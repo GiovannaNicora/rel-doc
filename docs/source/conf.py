bc005a8906db4d2a96e40a9f014c5c951cbe2c98
LorenzoPeracchio <124535474+LorenzoPeracchio@users.noreply.github.com>
2023-09-11 12:23:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-# html_theme_options = {
-#     "logo_only": True,
-# }
+html_favicon = "logo_relAI_Mcs_icon.ico"
 
 html_logo = 'logo_relAI_removebg.png'
 
